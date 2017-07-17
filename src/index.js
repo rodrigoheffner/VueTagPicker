4e46443d5be55a3dbd70cd9644ecd775ffd595f3
Andrew Durber <aidurber@gmail.com>
2017-07-17 11:32:02 +0100

    Re add VueTouch in install.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import VueTouch from 'vue-touch';
 module.exports = {
     TagPicker: require('./components/TagPicker.vue'),
-    install(Vue){
+    install(Vue) {
+        Vue.use(VueTouch)
         Vue.component('tag-picker', module.exports.TagPicker)
-    }    
+    }
 }
