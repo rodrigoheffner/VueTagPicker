4f44eaaf2a0cc81726ebad68003ee501b33c5162
Andrew Durber <aidurber@gmail.com>
2017-09-26 17:46:50 +0100

    More accurate coverage, don't include component installation and entry point

--- test/unit/specs/TagPicker.spec.js
+++ test/unit/specs/TagPicker.spec.js
@@ -2,6 +2,7 @@ import Vue from 'vue/dist/vue.js'
 import TagPicker from '@/components/TagPicker'
 import { CommaKey, TabKey, DeleteKey, SpaceKey, EnterKey } from '@/utils/constants';
 import { addClass, removeClass, toggleClass } from '@/utils/dom';
+
 describe('TagPicker.vue', () => {
   const basicStringArray = "foo|bar";
   let Constructor;
@@ -318,53 +319,4 @@ describe('TagPicker.vue', () => {
       done();
     });
   });
-});
-
-describe('utils', () => {
-  it('should add class to element', () => {
-    var element = { className: "" };
-    addClass(element, 'test-class');
-    expect(element.className.trim()).to.equal("test-class");
-  });
-
-  it('should add class to element and not override existing classes', () => {
-    var element = { className: "original-class" };
-    addClass(element, 'test-class');
-    expect(element.className.trim()).to.equal("original-class test-class");
-  });
-
-
-  it('should remove class from element', () => {
-    var element = { className: "test-class" };
-    removeClass(element, 'test-class');
-    expect(element.className.trim()).to.equal("");
-  });
-
-  it('should remove class from element and not remove any existing', () => {
-    var element = { className: "original-class test-class" };
-    removeClass(element, 'test-class');
-    expect(element.className.trim()).to.equal("original-class");
-  });
-
-
-  it('should remove all classes that match', () => {
-    var element = { className: "test-class test-class" };
-    removeClass(element, 'test-class');
-    expect(element.className.trim()).to.equal("");
-  });
-
-  it('should remove all classes that match and not remove any existing', () => {
-    var element = { className: "original-class test-class test-class" };
-    removeClass(element, 'test-class');
-    expect(element.className.trim()).to.equal("original-class");
-  });
-
-  it('should toggle class', () => {
-    var element = { className: "test-class" };
-    toggleClass(element, 'test-class');
-    expect(element.className.trim()).to.equal("");
-    toggleClass(element, 'test-class');
-    expect(element.className.trim()).to.equal("test-class");
-  });
-
 });
